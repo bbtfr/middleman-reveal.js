d233a55ec1419c89f3766cfa5ec9f92c66fe7475
Theo Li <bbtfrr@gmail.com>
2017-02-15 14:33:28 +0800

    fix application.js to use relative assets paths

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -4,9 +4,9 @@ Reveal.initialize({
 
   // More info https://github.com/hakimel/reveal.js#dependencies
   dependencies: [
-    { src: '/reveal.js/plugin/markdown/marked.js' },
-    { src: '/reveal.js/plugin/markdown/markdown.js' },
-    { src: '/reveal.js/plugin/notes/notes.js', async: true },
-    { src: '/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+    { src: 'reveal.js/plugin/markdown/marked.js' },
+    { src: 'reveal.js/plugin/markdown/markdown.js' },
+    { src: 'reveal.js/plugin/notes/notes.js', async: true },
+    { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
   ]
 });
